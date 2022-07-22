671c126bbb036280ef3f8d2888fc8e2c3251c080
Onur Usluca <onur-usluca>
2022-07-21 21:20:53 -0400

    First Commit

--- 03_SeleniumTestNG/src/test/java/students/Onur/lab01.java
+++ 03_SeleniumTestNG/src/test/java/students/Onur/lab01.java
@@ -1,7 +1,22 @@
 package students.Onur;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class lab01 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            WebDriverManager.chromedriver().setup();
+            WebDriver chrome = new ChromeDriver();
+            chrome.get("https://www.etsy.com/");
+            chrome.manage().window().maximize();
+            chrome.manage().window().minimize();
+
+            System.out.println("****************");
+
+            chrome.get("https://www.amazon.com/");
+            String title = chrome.getTitle();
+            System.out.println("Title of current page is = " + title);
 
     }
 }
