d821b13a7a432877d7ad36196998039ac4433e5a
ramazanozersf <brsheres@gmail.com>
2022-08-09 20:21:33 -0400

    Resta Assured 1 class 1. part

--- 06_API/src/test/java/code/GETStatus.java
+++ 06_API/src/test/java/code/GETStatus.java
@@ -1,11 +1,12 @@
 package code;
 
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 public class GETStatus {
+
+    String url = "https://simple-books-api.glitch.me";
 
     @Test
     void happyPathTest() {
@@ -13,7 +14,16 @@ public class GETStatus {
         Response response = RestAssured.get("https://simple-books-api.glitch.me/status");
 
         System.out.println("Response: " + response.asString());
+    }
 
+    @Test
+    (description = "validate status code")
+    void ValidateStatusCode(){
 
+        Response response = RestAssured.get(url+ "/status");
+
+        System.out.println(response.asString());
+
+        System.out.println(response.getStatusCode());
     }
 }
